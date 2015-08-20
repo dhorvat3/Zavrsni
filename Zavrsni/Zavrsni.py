e31151f79b06dd5ebc092b7638f066217d2def31
Davorin Horvat <davor-sb@hotmail.com>
2015-08-20 14:55:12 +0200

    Dodana grafika okoliša i puta.

--- Zavrsni/Zavrsni.py
+++ Zavrsni/Zavrsni.py
@@ -26,6 +26,11 @@ menuLvl1 = pygame.image.load('grafika/menuLvl1.png')
 menuLvl2 = pygame.image.load('grafika/menuLvl2.png')
 menuLvl3 = pygame.image.load('grafika/menuLvl3.png')
 menuHover = pygame.image.load('grafika/menuHover.png')
+okolis = pygame.image.load('grafika/mapa/trava_okolis.png')
+put_ravno = pygame.image.load('grafika/mapa/put_ravno.png')
+put_dole = pygame.image.load('grafika/mapa/put_dole.png')
+put_kutLD = pygame.image.load('grafika/mapa/put_kutLD.png')
+put_kutDD = pygame.image.load('grafika/mapa/put_kutDD.png')
 
 FPS = 30
 VisinaProzora = 640
@@ -93,13 +98,14 @@ def main ():
                     zgradaHP = lvlSeed.zgradaHP()
                     UI.postaviMrezu(grid)
                     UI.postaviNovce(startGold)
-                    lvl = Mapa(grid, VisinaProzora, SirinaProzora)
+                    lvl = Mapa(grid, VisinaProzora, SirinaProzora, okolis, put_ravno, put_dole, put_kutLD, put_kutDD)
                     tornjevi = []
                     GlZgrada = zgrada(POVRSINA, glavnaZgrada, zgradaHP, kraj, VisinaProzora, SirinaProzora, grid)
             if not kliknuto:
                 UI.hoverGlavniMenu(mousex, mousey)
             UI.crtajGlavniMenu()
         if mod == modIgra:
+            lvl.crtajMrezu(POVRSINA)
             if lvlSeed.vrijeme():
                 tip, brzina, HP = lvlSeed.vratiNeprijatelj()
                 if tip is not None:
@@ -120,7 +126,7 @@ def main ():
                 listaNeprijatelj[i].Pomakni()
             for i in range (len(listaNeprijatelj)):
                 NeprijateljiRect.append(listaNeprijatelj[i].ikonaRect)
-            lvl.crtajMrezu(POVRSINA)
+            
             if kliknuto:
                 lijevo, gore = UI.poljeNaPixelu(mousex, mousey)
                 if lijevo is None and gore is None:
--- Zavrsni/mapa.py
+++ Zavrsni/mapa.py
@@ -2,7 +2,7 @@
 
 class Mapa(object):
     """Klasa objekta mapa"""
-    def __init__(self, polje, visina, sirina):
+    def __init__(self, polje, visina, sirina, okolis, put_ravno, put_dole, put_kutLD, put_kut_DD):
         '''Stvaranje objekta mape, uzma polje po kojem generira mapu s obzirom na rezoluciju ekrana'''
         self.grid = polje
         self.brojRedova = len(self.grid)
@@ -10,6 +10,15 @@ class Mapa(object):
         print(self.brojRedova, self.brojStupaca)
         self.ekranVisina = visina
         self.ekranSirina = sirina
+        self.okolis = okolis
+        self.okolisRect = self.okolis.get_rect()
+        self.put_ravno = put_ravno
+        self.put_dole = put_dole
+        self.put_kutLD = put_kutLD
+        self.put_kutDD = put_kut_DD
+        self.put_kutDG = pygame.transform.rotate(self.put_kutDD, 180)
+        self.put_kutGD = pygame.transform.rotate(self.put_kutLD, 180)
+        self.putRect = self.put_ravno.get_rect()
     def crtajMrezu (self, POV):
         '''Crta mapu'''
         dimX = int(self.ekranVisina/self.brojRedova)
@@ -17,4 +26,28 @@ class Mapa(object):
         for i in range(self.brojRedova):
             for j in range(self.brojStupaca):
                 if self.grid[i][j] == 'S' or self.grid[i][j] == 'D' or self.grid[i][j] == 'L' or self.grid[i][j] == 'T':
-                    pygame.draw.rect(POV, (255, 15, 0), (dimX * j, dimY * i, dimX, dimY), 1)
+                    self.okolisRect.x = dimX * j
+                    self.okolisRect.y = dimY * i
+                    POV.blit(self.okolis, self.okolisRect)
+                    #pygame.draw.rect(POV, (255, 15, 0), (dimX * j, dimY * i, dimX, dimY), 1)
+                else:
+                    self.putRect.x = dimX * j
+                    self.putRect.y = dimY * i
+                    if i > 0 and i < 9:
+                        if self.grid[i + 1][j] == 'Z' or self.grid[i - 1][j] == 'Z':
+                            POV.blit(self.put_dole, self.putRect)
+                    if j > 0 and j < 9:
+                        if self.grid[i][j + 1] == 'Z' or self.grid[i][j - 1] == 'Z':
+                            POV.blit(self.put_ravno, self.putRect)
+                    if i < 9 and j > 0:
+                        if self.grid[i + 1][j] == 'Z' and self.grid[i][j - 1] == 'Z':
+                            POV.blit(self.put_kutLD, self.putRect)
+                    if i > 0 and j < 9:
+                        if self.grid[i - 1][j] == 'Z' and self.grid[i][j + 1] == 'Z':
+                            POV.blit(self.put_kutGD, self.putRect)
+                    if i > 0 and j > 0:
+                        if self.grid[i - 1][j] == 'Z' and self.grid[i][j - 1] == 'Z':
+                            POV.blit(self.put_kutDG, self.putRect)
+                    if i < 9 and j < 9:
+                        if self.grid[i + 1][j] == 'Z' and self.grid[i][j + 1] == 'Z':
+                            POV.blit(self.put_kutDD, self.putRect)
